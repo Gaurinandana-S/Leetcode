b128f2e7a8571db4b715376708adabd1fb04d439
Gaurinandana S <gaurinandanas4@gmail.com>
2026-02-28 14:43:22 +0530

    Time: 0 ms (100%), Space: 8.9 MB (7.01%) - LeetHub

--- 0050-powx-n/0050-powx-n.c
+++ 0050-powx-n/0050-powx-n.c
@@ -2,18 +2,17 @@ double myPow(double x, int n) {
     double pow_n=1;
     long long nn=n;
     int i;
+
+    if(nn<0){
+        x=1/x;
+        nn=-1*nn;
+    }
     while(nn>0){
         if (nn%2==1){
             pow_n*=x;
         }
         x*=x;
         nn/=2;
-    }
-    if(nn<0){
-        nn=-1*nn;
-        for(i=1;i<=nn;i++)
-            pow_n*=x;
-        pow_n=1/pow_n;
     }
     
     return pow_n;
