74fb0522942acea6a24bb8b6b63f91b30fc7ff3f
Gaurinandana S <gaurinandanas4@gmail.com>
2026-03-05 12:46:41 +0530

    Time: 147 ms (5.53%), Space: 20.4 MB (5.37%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.c
+++ 0283-move-zeroes/0283-move-zeroes.c
@@ -1,15 +1,15 @@
 void moveZeroes(int* nums, int numsSize) {
-    int* result=(int*)malloc(numsSize*sizeof(int));
-    int j=numsSize-1;
-    for (int i=0;i<numsSize;i++){
-        if(nums[i]==0)
-            result[j--]=nums[i];
-    }
-    for (int i=numsSize-1;i>=0;i--){
-        if(nums[i]!=0)
-            result[j--]=nums[i];
-    }
-    for(int i=0;i<numsSize;i++){
-        nums[i]=result[i];
+    int i,j,k,temp;
+    k=numsSize-1;
+    for(i=0;i<=k;i++){
+        if(nums[i]==0){
+            temp=nums[i];
+            for(j=i;j<k;j++){
+                nums[j]=nums[j+1];
+            }
+            nums[k]=temp;
+            k--;
+            i--;
+        }
     }
 }
